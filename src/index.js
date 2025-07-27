c31fcf14f2dd1014e167c98fdaba0d4fe801c5cf
Yabibi09 <bashasha@naver.com>
2025-07-27 17:16:29 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import App from './App';
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
